933755227402f3c198c7e55adb8201c003eb61eb
Andy Mascaro <amascaro1@hotmail.com>
2021-12-09 15:26:09 -0800

    Updated index for pics

---
+++ data.js
@@ -0,0 +1,13 @@
+export const characters = [{
+    name: 'Rock',
+    image: 'assets/avatars/rock.png',
+    audio: 'assets/sounds/car.mp3',
+}, {
+    name: 'Paper',
+    image: 'assets/avatars/paper.png',
+    audio: 'assets/sounds/rocket.mp3'
+
+}, { name: 'Scissors',
+    image: 'assets/avatars/scissors.png',
+    audio: 'assets/sounds/boat.mp3'
+}];
